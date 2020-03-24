fb2010eb0bde419113ad6da05a40efd0864994df
jugaltoppo <jugalsekhartoppo@gmail.com>
2020-03-24 10:48:15 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Home :message="message" />
+  </div>
+</template>
+
+<script>
+
+import Home from './components/Home.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      message: "welcome to my app"
+    }
+  },
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="todo">
+        <form @submit.prevent="addTodo" >
+            <input v-model="todo">
+            <button>Add todo</button>
+        </form>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>task</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+                    <tbody>
+                       <tr v-for="todo in todos" :key="todo.id"><td>{{todo.text}}</td> <td><button :value="todo.text" @click="removeTodo(todo)">X</button></td></tr>
+                   </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+       return {
+           todos: [],
+           todo: ""
+       }
+   },
+   methods : {
+       addTodo: function(){
+           if(this.todo.length>1){
+                this.todos.push({id: this.todos.length, text: this.todo});
+                this.todo="";
+           }
+       },
+       removeTodo: function(value){
+           let index=this.todos.filter((tdo)=>{
+               return tdo.id!==value.id
+           })
+           this.todos=index;
+       }
+   }
+}
+</script>
+
+<style>
+    #todo{
+        width: 30%;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: left;
+    }
+    form, table{
+        text-align: center;
+    }
+    table{
+        margin: 1rem auto;
+    }
+</style>
